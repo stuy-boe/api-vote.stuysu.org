5689e1cc38a371add7c88f213ae8a55aaf526549
Abir Taheer <abir488@gmail.com>
2019-12-01 23:12:35 -0500

    proxied front-end requests to react build

--- index.js
+++ index.js
@@ -33,13 +33,21 @@ app.use(session);
 app.use(cookieParser("some_semi_permanent_secret"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(morgan("dev"));
+app.use(morgan(process.env.MORGAN_FORMAT || "dev"));
 
-app.route("/").get((req,res) => {
-	res.send("Hello World!");
-});
+const handleDefaultNavigation = (req, res) => {
+	// TODO add in server side rendering
+	res.sendFile(path.join(__dirname, 'client/build/index.html'));
+};
+
+// Catch the index page before it is handled statically
+// Otherwise server side rendering doesn't happen
+app.route("/").get(handleDefaultNavigation);
 
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Fallback to react for non-static files
+app.route("*").get(handleDefaultNavigation);
 
 server.listen(app_port, () => {
 	console.log('listening on *:' + app_port);
