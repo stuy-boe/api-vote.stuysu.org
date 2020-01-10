b770ef2e56c79920a656f564cc9a4ef5cd345522
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2020-01-10 02:20:00 -0500

    added functionality to updateStudents endpoint

--- routes/api/talos/updateStudents.js
+++ routes/api/talos/updateStudents.js
@@ -1,10 +1,28 @@
 const Talos = require("talos-js");
 const {Students} = require("./../../../config/database");
 
+let addOrUpdateStudents = (email, grade) => new Promise(resolve => {
+	Students
+		.findOne({ where: {email} })
+		.then(res => {
+			// update existing students
+			if(res){
+				if(String(res.grade) !== String(grade)) {
+					res.update({email, grade});
+					return resolve(1);
+				}
+
+				return resolve(0);
+			}
+
+			Students.create({email, grade});
+			return resolve(1);
+		});
+});
+
 module.exports = ["/api/talos/updateStudents", async (req, res) => {
 	const username = req.body.username || "";
 	const password = req.body.password || "";
-
 	const user = new Talos( username, password);
 
 	if(! await user.authenticate())
@@ -15,6 +33,25 @@ module.exports = ["/api/talos/updateStudents", async (req, res) => {
 
 	let all_students = await user.getAllStudents("");
 
-	res.json(all_students);
+	let updatePromises = [];
 
+	for(let x = 0; x < all_students.length ; x++){
+		try {
+			let student = all_students[x];
+			let promise = addOrUpdateStudents(student.user.email, student.grade);
+			updatePromises.push(promise);
+		} catch(er) {
+			console.log(er);
+		}
+	}
+
+	Promise.all(updatePromises).then(values => {
+		let rows_affected = values.reduce((a, b) => {
+			if(typeof b === "number")
+				return a + b;
+			return a;
+		});
+		res.json({success: true, rows_affected});
+	})
+		.catch(() => res.json({success: false}));
 }];
