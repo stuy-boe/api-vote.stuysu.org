e8dd3a6fc766bb77ace3d95b5038ff7bea9b7bda
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2020-01-19 00:04:10 -0500

    fixed route joining for s3 redirect

--- routes/s3.js
+++ routes/s3.js
@@ -1,8 +1,14 @@
 const url = require("url");
+const path = require("path");
 const router = require("express").Router();
 
 router.get("*", (req, res) => {
-	res.status(301).redirect(url.resolve((process.env.S3_URI || "/storage"),  req.path));
+
+	// Redirect the user to the appropriate object on our s3 service
+	let s3_url = url.parse(process.env.S3_URL || "/storage");
+	s3_url.pathname = path.join(s3_url.pathname, req.path);
+
+	res.status(301).redirect(url.format(s3_url));
 });
 
 module.exports = router;
