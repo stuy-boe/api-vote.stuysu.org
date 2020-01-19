8665c89e269f155359071e86a85be85be2239ca6
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2020-01-18 19:49:40 -0500

    made image urls absolute when possible

--- opengraph/elections.js
+++ opengraph/elections.js
@@ -1,22 +1,29 @@
 const router = require("express").Router();
+const url = require("url");
 const {Elections} = require("./../database");
 
 router.get("/elections", (req, res, next) => {
-	req.og.title = "All Elections";
+
+	req.og.title = "All Elections | Stuy BOE Voting Site";
 	req.og.description = "View results of elections from the past as well as up to date information about current elections.";
 	next();
 });
 
 router.get("/elections/:public_url", async (req, res, next) => {
+
 	let election = await Elections.findOne({where: {public_url: req.params.public_url}});
+
 	if(election){
-		req.og.title = `${election.name} | Election`;
+
+		req.og.title = `${election.name} | Stuy BOE Voting Site`;
 		req.og.description = `Overview of ${election.name}. Vote, view updates, learn about candidates and more.`;
-		req.og.image = election.picture;
+		req.og.image = url.resolve(process.env.PUBLIC_URL || "", election.picture);
 	} else {
+
 		req.og.title = "Election not found";
 		req.og.description = "There is no election at that url... yet!";
 	}
+
 	next();
 });
 
--- opengraph/index.js
+++ opengraph/index.js
@@ -5,9 +5,9 @@ const htmlEntities = require("./../tools/htmlEntities");
 router.use("*", (req, res, next) => {
 	req.og = {};
 	req.og.site_name = "Stuy Board of Elections Voting Site";
-	req.og.title = "Error 404 | Page Not Found";
+	req.og.title = "Error 404 - Page Not Found | Stuy BOE Voting Site";
 	req.og.type = "website";
-	req.og.image = "/logo512.png";
+	req.og.image = url.resolve(process.env.PUBLIC_URL || "", "/logo512.png");
 	req.og.description = "This page does not exist or has been moved";
 	req.og.url = url.resolve(process.env.PUBLIC_URL || "", req.path);
 
@@ -24,7 +24,7 @@ router.use("*", (req, res, next) => {
 });
 
 router.use("/", (req, res, next) => {
-	req.og.title = "Home";
+	req.og.title = "Home | Stuy BOE Voting Site";
 	req.og.description = "This is where voting as well as campaigning for Student Union Elections takes place";
 	next();
 });
