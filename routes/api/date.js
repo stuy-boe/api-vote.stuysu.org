2fdf0665b520f3109796187643ddc31021082487
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2020-04-09 17:44:58 -0400

    Extract date to separate api (#23) (#24)
    * fixed build image
    
    * moved date to /api/date
    
    * fixed issue with not exporting router

---
+++ routes/api/date.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+
+	res.json({
+		success: true,
+		payload: {
+			date: new Date().toISOString()
+		}
+	});
+
+});
+
+module.exports = router;
--- routes/api/state.js
+++ routes/api/state.js
@@ -2,23 +2,20 @@ const router = require("express").Router();
 
 router.get("/", (req, res) => {
 
-	const date = new Date().toISOString();
-
-	if( ! req.session.signedIn )
+	if( ! req.session.signedIn ) {
 		return res.json({
 			success: true,
 			payload: {
 				signedIn: false,
-				date
 			}
 		});
+	}
 
 	// TODO UPDATE ADMIN AND CAMPAIGNING WITH REAL VALUES
 	return res.json({
 		success: true,
 		payload: {
 			signedIn: true,
-			date,
 			user: {
 				email: req.session.email,
 				name: req.session.name
