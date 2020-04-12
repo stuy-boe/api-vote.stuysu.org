a61a453ccba5ad40af4ef4a6628aa74b8bfaaff4
Abir Taheer <abir@taheer.me>
2020-04-12 01:12:33 -0400

    switched cors url matching to regex

--- routes/cors.js
+++ routes/cors.js
@@ -1,11 +1,11 @@
 const cors = require("cors");
 const RefusalError = require("./../utils/RefusalError");
 
-const allowedOrigins = (process.env.ALLOWED_ORIGINS || "http://localhost:3000 http://localhost:3001").split(" ");
+const allowedOrigins = new RegExp(process.env.ALLOWED_ORIGINS || /^(http(s?):\/\/localhost(:\\d*?))$/gi);
 const corsOptions = {
 	origin: (origin, callback) => {
 
-		if (! origin || allowedOrigins.includes(origin) || process.env.NODE_ENV === "development")
+		if (! origin || origin.match(allowedOrigins) || process.env.NODE_ENV === "development")
 			callback(null, true);
 		else
 			callback(new RefusalError("Not allowed by CORS", "ERR_HTTP2_INVALID_ORIGIN"));
