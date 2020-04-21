dfc122c8fc24c14272363622168e600cf6136847
Abir Taheer <abir@taheer.me>
2020-04-21 02:50:33 -0400

    added auth testing to admin route handlers

--- routes/api/admin/elections/index.js
+++ routes/api/admin/elections/index.js
@@ -1,4 +1,23 @@
 const router = require('express').Router();
+const RefusalError = require('./../../../../utils/RefusalError');
+const { adminPrivileges } = require('./../../../../database');
+
+// Only admins with the 'elections' privilege can access the following endpoints
+router.use(async (req, res, next) => {
+	const hasAdminPrivilege = await adminPrivileges.exists(
+		req.session.email,
+		'elections'
+	);
+
+	if (!hasAdminPrivilege) {
+		throw new RefusalError(
+			"You don't have permission to use this endpoint.",
+			'INSUFFICIENT_PERMISSIONS'
+		);
+	}
+
+	next();
+});
 
 router.use('/pics', require('./pics'));
 router.use('/create', require('./create'));
--- routes/api/admin/index.js
+++ routes/api/admin/index.js
@@ -1,4 +1,16 @@
 const router = require('express').Router();
+const RefusalError = require('./../../../utils/RefusalError');
+
+// Only authenticated users can use the /admin endpoint
+router.use((req, res, next) => {
+	if (!req.session.signedIn) {
+		throw new RefusalError(
+			'You need to be authenticated to use this endpoint.',
+			'AUTH_REQUIRED'
+		);
+	}
+	next();
+});
 
 router.use('/elections', require('./elections'));
 
