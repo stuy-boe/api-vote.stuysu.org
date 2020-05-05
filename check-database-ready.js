17b90e2bf907c87afcffdc62f1ccc5416cadbd73
Abir Taheer <abir@taheer.me>
2020-05-05 12:32:45 -0400

    fixed port checker

--- check-database-ready.js
+++ check-database-ready.js
@@ -6,9 +6,9 @@ const checkMysqlReady = iteration => {
 	}
 
 	// Checks the status of a single port
-	portscanner.checkPortStatus(3306, '127.0.0.1', function (error, status) {
+	portscanner.checkPortStatus(3001, '127.0.0.1', function (error, status) {
 		// Status is 'open' if currently in use or 'closed' if available
-		if (status === 'open') {
+		if (status === 'closed') {
 			setTimeout(checkMysqlReady, 2000, iteration + 1);
 			console.log('MySQL not ready, trying again in 2 seconds');
 		} else {
