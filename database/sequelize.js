a156cf35f8f936a35c36aee903c7d2bff262e5b6
Abir Taheer <abir@taheer.me>
2020-05-25 09:26:23 -0400

    disabled sequelize logging for CI

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const databaseFile = path.resolve(__dirname, './../app.db');
 
+// Logging might become bothersome in CI and so we can selectively disable it in development
+const logging = process.env.CI === 'true' ? false : console.log;
+
 module.exports = {
 	development: {
 		url: process.env.SEQUELIZE_URL || `sqlite::${databaseFile}`,
@@ -9,7 +12,8 @@ module.exports = {
 			collate: 'utf8_unicode_ci'
 		},
 		ssl: true,
-		native: true
+		native: true,
+		logging
 	},
 	production: {
 		url: process.env.SEQUELIZE_URL,
