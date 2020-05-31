5a113f6aec77e6756b5454b954f904e3a7d3802a
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2020-05-30 21:49:05 -0400

    Add tests for database models (#63)
    * added tests for admin privileges
    
    * added tests for election
    
    * added tests for student model
    
    * fixed formatting

---
+++ test/database/models/test-students.js
@@ -0,0 +1,29 @@
+const { students } = require('./../../../database');
+const { expect } = require('chai');
+
+describe('students', () => {
+	const grade = -1;
+	const email = 'email@example.com';
+	let fakeStudent;
+
+	before(async () => {
+		fakeStudent = await students.create({
+			email,
+			grade
+		});
+	});
+
+	describe('#getGrade', () => {
+		it("should return the student's grade", async () => {
+			expect(await students.getGrade(email)).to.equal(grade);
+		});
+
+		it("should return null if an email doesn't exist", async () => {
+			expect(await students.getGrade('')).to.be.null;
+		});
+	});
+
+	after(async () => {
+		fakeStudent.destroy();
+	});
+});
