21c6f2db47e6d734bcfbe095d605fcf2914d68a3
Abir Taheer <abir@taheer.me>
2020-09-10 04:37:31 -0400

    fixed default url creation

--- src/graphql/resolvers/CloudinaryPicture/defaultUrl.js
+++ src/graphql/resolvers/CloudinaryPicture/defaultUrl.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 module.exports = pic => {
-	return cloudinary.url({
-		public_id: pic.publicId,
+	return cloudinary.url(pic.publicId, {
 		secure: true,
-		quality: 'auto'
+		quality: 'auto',
+		flags: 'lossy'
 	});
 };
