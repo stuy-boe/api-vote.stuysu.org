20606abc7712c40c7f9f897d4927295a18093a2e
Abir Taheer <abir@taheer.me>
2020-09-13 07:25:40 -0400

    fixed querying of updates

--- src/graphql/resolvers/Query/updates.js
+++ src/graphql/resolvers/Query/updates.js
@@ -2,5 +2,15 @@ const mongoose = require('mongoose');
 const Update = mongoose.model('Update');
 
 module.exports = (root, { electionId, candidateId }) => {
-	return Update.find({ electionId, candidateId });
+	const queryObj = {};
+
+	if (electionId) {
+		queryObj.electionId = electionId;
+	}
+
+	if (candidateId) {
+		queryObj.candidateId = candidateId;
+	}
+
+	return Update.find(queryObj);
 };
